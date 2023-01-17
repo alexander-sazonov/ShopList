f2b4b6bcd340c8834cd5e4cbed74401875d18333
alexander <alexandersznov@gmail.com>
2023-01-17 13:07:13 +0300

    Initial commit

---
+++ app/src/main/java/com/sazonov/shoplist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sazonov.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sazonov.shoplist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
